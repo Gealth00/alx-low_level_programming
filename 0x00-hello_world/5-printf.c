268cd788821dd00b05b3690a3e6cd060deceb46c
Gealth00 <gealth00@gmail.com>
2022-09-08 15:03:25 -0700

    Exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio>
+/* *main- Serves as entry point
+ * *Return- By defult returns zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+return 0;
+}
