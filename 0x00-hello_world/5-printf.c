6cc12987df58510ed53ef7e5838cd55dfc2ab13b
Gealth00 <gealth00@gmail.com>
2022-09-08 15:21:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /* *
  * main- Serves as entry point for the program.
  *
- * *Return: By default returns zero if no error
+ * *Return:0 if no errors, returns non zero uf errors
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 return 0;
 }
