84ee1176406444680baba80900f310ee0710b1f9
Gealth00 <gealth00@gmail.com>
2022-09-08 14:40:24 -0700

    Sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-/* #!/bin/bash 
- * Program set to print out a sentence 
- *
-*/
 #include <stdio.h>
+/* #!/bin/bash                                                  
+ *
+ * Program set to print out a sentence
+ * Return type is void
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
