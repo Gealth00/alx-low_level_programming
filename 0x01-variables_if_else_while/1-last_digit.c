3cac6c6a1504da6cc420833762e6b2ef3c46865a
Gealth00 <gealth00@gmail.com>
2022-09-09 14:45:00 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-#nclude <stdio.h>
+#include <stdio.h>
 /* betty style doc for function main goes there */
 /**
  * main- entry for variable if else statement
