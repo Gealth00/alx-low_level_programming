f0ba6245f46fb56e51c22a6d88be5a69dcbdf6e2
Gealth00 <gealth00@gmail.com>
2022-09-10 06:23:00 -0700

    n

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  * main - program that prints all digit numbers of base 10 starting from
+ *   * All your code should be in the main function
+ *    * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++);
+	putchar(d);
+	putchar("\n");
+	return (0);
+}
