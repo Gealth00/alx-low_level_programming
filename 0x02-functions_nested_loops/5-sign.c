78f3fb6673ff0c7b7f5731321ff435a1f336606c
Gealth00 <gealth00@gmail.com>
2022-09-14 02:01:56 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,18 +8,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		printf('+');
-	return (1);
-	else if
 	{
-		n == 0;
-	printf('0');
+		printf("+");
+	return (1);
+	}
+	else if (n == 0)
+	{
+	printf("0");
 	return (0);
 	}
-	else if
+	else if (n < 0)
 	{
-		n < 0;
-	printf('-');
+	printf("-");
 	return (1);
 	}
 	else
