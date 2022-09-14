5a0531804b43e09e7821f7c7fcba8e0d9a658d99
Gealth00 <gealth00@gmail.com>
2022-09-14 14:42:16 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -35,7 +35,7 @@ void times_table(void)
 			else if ((product < 10) && (y != 0))
 			{
 				_putchar(' ');
-				_putchar((produce % 10) + '0');
+				_putchar((product % 10) + '0');
 			}
 		}
 		_putchar('\n');
