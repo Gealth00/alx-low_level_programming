226a99a5735b10d1319f94fa566ccad59e213d9f
Gealth00 <gealth00@gmail.com>
2022-09-18 13:43:40 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_most_numbers - prints the numbers 0-9
@@ -7,7 +6,8 @@
  * Return: Always 0.
 */
 
-void print_most_numbers(void)
+void print_most_numbers(void);
+
 {
 	char i;
 
