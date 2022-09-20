46dc6350e54afb55ff085e1763fa1200e4c14ba1
Gealth00 <gealth00@gmail.com>
2022-09-20 13:06:30 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * print_rev(char *s) - print string in reverse
+ *
+ * return: always 0
+*/
+
+void print_rev(char *s);
+{
+	int i = 0;
+	int r;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+		_putchar(r[i])
+			s--;
+		_putchar('\n');
+}
