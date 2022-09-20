6292e5e9a394628fd64458287d332b196565092c
Gealth00 <gealth00@gmail.com>
2022-09-20 14:40:25 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: varaible
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i--; i >= 0; i--)
+	_putchar(s[i]);
+}
