759ed916e4f8bb36bc41c94aa127af932d4e7752
Gealth00 <gealth00@gmail.com>
2022-09-20 06:17:06 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: string
+ * Return: return length
+*/
+
+int _strlen(char *s);
+{
+	char s;
+	int len;
+
+	len = _strlen(s);
+	return (len);
+}
+
