9f6ff8746ded29182cfd7f4d74319b0b13c12936
Gealth00 <111291314+Gealth00@users.noreply.github.com>
2022-10-03 00:23:45 +0100

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints its name followed by a new line
  * @argc: number of command line arguments
@@ -6,7 +7,7 @@
  * Return: 0 - success.
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	
 	printf("%s\n", argv[0]);
