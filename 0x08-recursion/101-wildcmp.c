511711dcbcaedf6eb6953365e82440b84cadbd10
Gealth00 <111291314+Gealth00@users.noreply.github.com>
2022-10-02 23:32:52 +0100

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,57 +1,20 @@
 #include "main.h"
 
-int check_wildcmp(char *s);
-
 /**
- * wildcmp - Returns if a string is wildcmp
- * @s: the string value to be checked
- *
- * Return: integer value
+ * wildcmp - compares two strings.
+ * @s1: string 1.
+ * @s2: string 2. It can contains a * as a special character.
+ * Return: 1 if are identical, 0 if not.
  */
-int wildcmp(char *s)
+int wildcmp(char *s1, char *s2)
 {
-if (*s == '0')
-return (1);
-
-return (check_wildcmp(s));
-}
-
-/**
- * check_wildcmp - check if a string is wildcmp
- * @s: the string value to be checked
- * Return: integer value
- */
-int check_wildcmp(char *s)
-{
-int l = _strlen_recursion(s) - 1;
-
-
-if (*s == s[l])
-{
-s++;
-l--;
-}
-else
-{
-return (0);
-}
-
-return (1);
-}
-
-/**
- * _strlen_recursion - Get the length of a string
- * @s: the string to get the length
- *
- * Return: the string length
- */
-int _strlen_recursion(char *s)
-{
-if (*s == '\0')
-{
-return (0);
-}
-
-s++;
-return (_strlen_recursion(s) + 1);
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
